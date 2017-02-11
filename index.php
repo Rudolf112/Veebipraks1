a717f8098428a97f0fbf8ef6c77477647d235de7
Rudolf Purge <rpurge@itcollege.ee>
2017-02-11 15:18:48 +0200

    Teine commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Täiega vägev HTML leht</title>
+<link rel="stylesheet" type="text/css" href="stiil.css">
+</head>
+<body>
+
+<h1>Minu esimene HTML!</h1>
+<p>Täiega vägev värk. Viimati sai HTML'i tehtud 100a tagasi.</p>
+<?php
+echo phpversion();
+?>
+<img src="keep-calm-and-windows-rules-2.jpg" alt="W3Schools.com" width="104" height="142">
+
+</body>
+</html>
